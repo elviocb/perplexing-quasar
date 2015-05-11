7bfd1c34d65d8d58e3cf9c0c295c0f16e3e1a08a
Elvio Cavalcante <elvio@madfx.com.br>
2015-05-11 15:38:26 -0700

    Server allows CORS

--- server/server.js
+++ server/server.js
@@ -30,6 +30,13 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Allow CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // SET DEFAULT ROUTES
 app.use('/', index);
 // SET INTEGRATION ROUTES 
